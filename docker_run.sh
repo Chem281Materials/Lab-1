a632faa1e6fea9916fbbc341517d3672e215ea94
ujamshed <69454662+ujamshed@users.noreply.github.com>
2025-09-01 15:47:31 -0400

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix vim-x11-lab
+docker run -it --rm -e DISPLAY=$DISPLAY --net=host -v /tmp/.X11-unix:/tmp/.X11-unix vim-x11-lab
